267fff69237cdb7ed5bbe1726b3c62ba99eb7097
Anton Fischer <anton.fischer.18@gmail.com>
2026-08-07 15:30:07 +0200

    add route with param forwarding

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,34 @@
+const url = require("url");
 const express = require("express");
 const router = express.Router();
+const needle = require("needle");
 
-router.get("/", (req, res) => {
-    res.json({ success: true });
+// include env vars
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_KEY_NAME = process.env.API_KEY_NAME;
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
+
+router.get("/", async (req, res) => {
+    try {
+        const reqParams = url.parse(req.url, true).query;
+        console.log("Received request with params:", reqParams);
+
+        const params = new URLSearchParams({
+            [API_KEY_NAME]: API_KEY_VALUE,
+            ...reqParams,
+        });
+
+        const reqUrl = `${API_BASE_URL}?${params}`;
+        console.log("Forwarding API-request:", reqUrl);
+
+        const apiRes = await needle("get", reqUrl);
+        const data = apiRes.body;
+
+        //console.log("Sending API-response:", data);
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error });
+    };
 });
 
 module.exports = router;
