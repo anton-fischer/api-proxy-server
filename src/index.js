5894d97d9ee7f6c6fe65654d4466f200aee50386
Anton Fischer <anton.fischer.18@gmail.com>
2026-08-07 14:57:00 +0200

    add basic route

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+
+// use port declared in env, else use 5000
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+// include routes
+app.use("/api", require("./routes/routes"))
+
+// enable cors
+app.use(cors());
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
